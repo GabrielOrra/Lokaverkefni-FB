01bf445db4f09873e0bbcbec74c0062590784ca8
GabrielOrra <gabrielorra@gmail.com>
2026-02-11 19:15:08 +0000

    switch to laptop

---
+++ keppnisforritunardæmi/skilabod.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+
+int main() {
+
+
+
+    std::vector<int> xCoords;
+    int numOfCoords;
+    std::cin >> numOfCoords;
+    for (int i = 0; i < numOfCoords; i++) {
+
+    }
+
+    return 0;
+}
