8414511cc75e9949be06ce3e36208c2f49f16f66
GabrielOrra <gabrielorra@gmail.com>
2026-02-11 00:29:43 +0000

    failed dæmi

---
+++ keppnisforritunardæmi/heiltolusumma(unfinished).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    long long n;
+    std::cin >> n;
+    long long result;
+
+    if (n >= 1) {
+        result = n * (n + 1) / 2;
+    }
+    else if (n == -1) {
+        result = 0
+    }
+    else {
+        result = 1 + n * (n + 1) / 2;
+    }
+    
+    std::cout << result;
+
+    return 0;
+}
