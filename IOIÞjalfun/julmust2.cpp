9b09672e086dcb81f4ffd1ea2a1efa91a5a89b5b
GabrielOrra <gabrielorra@gmail.com>
2026-03-24 00:18:29 +0000

    move to pc

---
+++ IOIÞjalfun/julmust2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+
+    int l = 1;
+    int h;
+    std::cin >> h;
+    while (l <= h) {
+
+        int m = (l+h) / 2;
+        // int m = l + (h - l) / 2;
+        std::cout << m << '\n' << std::flush; 
+
+        std::string answer;
+        std::cin >> answer;
+
+        if (answer == "lower") {
+            h = m - 1;
+        } else if (answer == "higher") {
+            l = m + 1;
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
